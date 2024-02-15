d4fb2d0c6d9e29121ae1d9b0092e8de4bddf6814
Enes <enes32y@gmail.com>
2024-02-15 12:58:45 -0800

    mostly implemented DDAMFN, need to fix input shape error. will fix soon

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # emotion-chatbot
 SJSU 2024 Grad Project - Emotion Chatbot
+
+
+#DDAMFN REQUIREMENTS:
+Install python 3.8.9: https://www.python.org/downloads/release/python-389/ 
+Install pytorch:
+
+# CUDA 11.1
+pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+# CUDA 10.2
+pip install torch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0
+
+# CPU only
+pip install torch==1.8.0+cpu torchvision==0.9.0+cpu torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+
+Download DDAMFN Repository: https://github.com/simon20010923/DDAMFN
+Paste ./networks/ and ./pretrained/ into the main code folder (same folder as main.py)
+
+#To run script:
+python main.py
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import tensorflow as tf
-from tensorflow import keras
-# importing OpenCV library
 import cv2
 import numpy as np
 import os  
+import sys
+import argparse
 
+##DDAMFN Libraries
+import torch
+from torchvision import transforms, datasets
+import torch.utils.data as data
+from networks.DDAM import DDAMNet
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--raf_path', type=str, default='/data/rafdb/', help='Raf-DB dataset path.')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size.')
+    parser.add_argument('--workers', default=8, type=int, help='Number of data loading workers.')
+    parser.add_argument('--num_head', type=int, default=2, help='Number of attention head.')
+    parser.add_argument('--model_path', default = './checkpoints/rafdb.pth')
+    return parser.parse_args()
 
 def capture_and_evaluate_frames(model, frame_cap = None, width=224, height=224, cam_port=0):
     print("loading camera. This takes a few seconds..")
@@ -23,7 +36,11 @@ def capture_and_evaluate_frames(model, frame_cap = None, width=224, height=224, 
             captured_frames+=1
             image = cv2.resize(image, (width,height), interpolation = cv2.INTER_AREA)
             image = np.expand_dims(image, axis=0)
-            prediction = model.evaluate(image) 
+            
+            #make prediction with DDAMFN
+            
+            tensor = torch.from_numpy(image)
+            prediction = model(tensor)
             print("Model predicted: " + str(prediction))
             
             if frame_cap is not None and frame_cap <= captured_frames:
@@ -31,13 +48,19 @@ def capture_and_evaluate_frames(model, frame_cap = None, width=224, height=224, 
     
 
 def main():
-    print("Running TF Version: " + str(tf.__version__))
-    model_folder = "./PAtt-Lite-main/PAtt-Lite-main/pretrained/"
-    model_name = "rafdb.h5"
+    class_names = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Angry']  
+
+    #obtain model from: https://github.com/simon20010923/DDAMFN
+    model_folder = "./DDAMFN-main/DDAMFN-main/pretrained/"
+    model_name = "MFN_msceleb.pth"
     
-    patt_lite = keras.models.load_model(model_folder+model_name)
+    args = parse_args()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = DDAMNet(num_class=7,num_head=args.num_head)
+    model.to(device)
+    model.eval()   
     
-    capture_and_evaluate_frames(patt_lite, frame_cap = 10)
+    capture_and_evaluate_frames(model, frame_cap = 10)
     
     
 if __name__ == "__main__":
