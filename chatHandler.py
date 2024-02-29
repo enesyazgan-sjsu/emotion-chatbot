e80613de5be862cb58d6721b5ef2ba1898e70c15
emead00 <emead00@users.noreply.github.com>
2024-02-28 22:47:36 -0800

    Add files via upload

---
+++ chatHandler.py
@@ -0,0 +1,36 @@
+import os
+from openai import OpenAI
+
+class ChatHandler:
+    def __init__(self):
+        self.message = 'Say this is a test.'
+        self.role = 'user'
+        self.model = 'gpt-3.5-turbo'
+        self.api_key = ''
+        self.client = None
+        self.chatCompletion = None
+        self.reply = None
+        
+    def initializeAPI(self, api_key = "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"):
+        self.api_key = api_key
+        self.client = OpenAI(api_key = self.api_key)
+        self.api_key = '' # deleted for security concerns
+
+    def defineMessage(self, message='Say this is a test.'):
+        self.message = {"role":self.role, "content":self.message}
+    def defineRole(self, role='user'):
+        self.role = role
+    def defineModel(self, model = 'gpt-3.5-turbo'):
+        self.model = model
+
+    def sendMessage(self):
+        self.chatCompletion = self.client.chat.completions.create(messages=[{"role":"user","content":"Say this is a test"}],
+                                                                  model="gpt-3.5-turbo")
+        return (self.chatCompletion)
+    def returnReply(self):
+        return(self.chatCompletion.choices[0].message.content)
+
+x = ChatHandler()
+x.initializeAPI()
+x.sendMessage()
+print(x.returnReply())
