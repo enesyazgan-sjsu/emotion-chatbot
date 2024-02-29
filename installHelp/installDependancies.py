10f6782e67883fed264dc0dcdf7b85454b49d68c
emead00 <emead00@users.noreply.github.com>
2024-02-28 21:50:38 -0800

    Update installDependancies.py

--- installHelp/installDependancies.py
+++ installHelp/installDependancies.py
@@ -3,12 +3,11 @@ import sys
 def install(name):
     subprocess.call([sys.executable, '-m', 'pip', 'install', name])
 
-installList = ['opencv-python', 'pillow', 'torch', 'torchvision', 'facenet_pytorch', 'networks']
+installList = ['opencv-python', 'pillow', 'torch', 'torchvision', 
+	       'facenet_pytorch', 'networks', 'asyncore', 'asynchat',
+	      'socket', 'openai', 'python-dotenv']
 
-installList.append('asyncore')
 installList.append('networks.DDAM')
-installList.append('asynchat')
-installList.append('socket')
 
 for each in installList:
 	install(each)
