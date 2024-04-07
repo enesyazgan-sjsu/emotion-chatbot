e813065e065f6db3f043fe440b9d033bb1b93687
emead00 <emead00@users.noreply.github.com>
2024-04-06 22:14:43 -0700

    Add files via upload
    data class and tempData file for testing

--- evalGui.py
+++ evalGui.py
@@ -22,10 +22,73 @@ except:
 
 PATH_TO_DATA = './test.mp4'# "C:\\Users\\emead\\Downloads\\test.mp4"
 
+# DATA class for video, queries, timestamps, responses
+class DATA:
+    def __init__(self, path = None):
+        self.dataDict = {} # timestampStart is the integer part of the time start 
+        # {"timestampStart" : {'vidPath'   : "",\
+        #                      'origQuery' : "",\
+        #                      'augQuery'  : "",\
+        #                      'origResponse' : "",\
+        #                      'augResponse'  : ""} }
+        self.delimElement = '||'
+        self.delimKeyVal = '|+|'
+        if path == None:
+            self.readData(path)
+    def addData(self, ts, vp, oq, aq, orr, ar):
+        self.dataDict[ts] = {'vidPath' : vp, 'origQuery' : oq,\
+                             'augQuery' : aq, 'origResponse' : orr,\
+                             'augResponse' : ar}
+    def printData(self, ts = None):
+        if ts == None:
+            for each in self.dataDict.keys():
+                print(each, self.dataDict[each])
+        else:
+            print(self.dataDict[ts])
+    def removeData(self, ts):
+        del self.dataDict[ts]
+    def saveData(self, savePath = './tempDataSave'):
+        with open(savePath, "w") as f:
+            for timestamp in self.dataDict.keys():
+                stringToWrite = "timestampStart" + self.delimKeyVal + timestamp
+                for element in self.dataDict[timestamp].keys():
+                    temp = self.dataDict[timestamp][element]
+                    stringToWrite = stringToWrite + self.delimElement + element
+                    stringToWrite = stringToWrite + self.delimKeyVal + temp
+                stringToWrite = stringToWrite+'\n'
+                f.write(stringToWrite)
+    def readData(self, readPath = './tempDataSave'):
+        self.dataDict = {}
+        with open(readPath, 'r') as f:
+            for line in f:
+                tempDict = {}
+                for keyVal in line.split(self.delimElement):
+                    kv = keyVal.split(self.delimKeyVal)
+                    if kv[0] == 'timestampStart':
+                        ts = kv[1]
+                    else:
+                        tempDict[kv[0]]=kv[1]
+                self.dataDict[ts]=tempDict
+    
+'''
+x = DATA()
+x.addData('45','./','hello','hello(happy)','yes?','you seem happy!')
+x.addData('46','./','hello','hello(happy)','yes?','you seem happy!')
+x.printData()
+x.saveData()
+x.removeData('46')
+print()
+x.printData()
+x.readData()
+print()
+x.printData()
+'''
 
-# GUI class for evaluation
+# GUI_EVAL class for evaluation
 class GUI_EVAL:
     def __init__(self, chatWinWidth = 400, chatWinHeight = None, minHeight = 10, ratingScale = 10):
+        self.data = DATA('./tempDataSave')
+        
         if chatWinHeight == None:
             chatWinHeight = chatWinWidth * .75
         if chatWinHeight <= 0:
@@ -240,7 +303,6 @@ class GUI_EVAL:
 
 def main():
     evalWin = GUI_EVAL()
-    pass
 
 if __name__ == "__main__":
     main()
